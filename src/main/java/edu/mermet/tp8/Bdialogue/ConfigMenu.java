141c2ef7c9c36e6f97aefb34b5b2b5f5acf81c68
Hakim ouatar <hakimouatar6@gmail.com>
2021-03-26 15:13:37 +0100

    added action listener to VALIDER

--- src/main/java/edu/mermet/tp8/Bdialogue/ConfigMenu.java
+++ src/main/java/edu/mermet/tp8/Bdialogue/ConfigMenu.java
@@ -1,14 +1,25 @@
 package edu.mermet.tp8.Bdialogue;
 
+import edu.mermet.tp8.Application;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ConfigMenu extends JDialog {
+    private JButton valider;
+    private JButton annuler;
+    private ButtonGroup grp ;
+    private ButtonGroup grpS;
+    private ButtonGroup grpD ;
+    private ButtonGroup grpB ;
+    private Application application;
 
-
-    public ConfigMenu(JFrame parent){
+    public ConfigMenu(Application parent){
         super(parent, "Configuration des menus",true);
+        application=parent;
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setSize(500, 180);
         setLocationRelativeTo(null);
@@ -45,38 +56,55 @@ public class ConfigMenu extends JDialog {
         JLabel diaporama = new JLabel("Diaporama",JLabel.RIGHT);
         JLabel boutons  = new JLabel("Boutons",JLabel.RIGHT);
         //create RadioButton
-        ButtonGroup grp = new ButtonGroup();
-        ButtonGroup grpS = new ButtonGroup();
-        ButtonGroup grpD = new ButtonGroup();
-        ButtonGroup grpB = new ButtonGroup();
+         grp = new ButtonGroup();
+         grpS = new ButtonGroup();
+         grpD = new ButtonGroup();
+         grpB = new ButtonGroup();
+         // conversion
         JRadioButton auto = new JRadioButton("Auto");
+        auto.setActionCommand("auto");
+        auto.setSelected(true);
         JRadioButton afficher = new JRadioButton("Affichée");
+        afficher.setActionCommand("afficher");
         JRadioButton cacher = new JRadioButton("Cachée");
+        cacher.setActionCommand("cacher");
 
-
+      //saisie
         JRadioButton Sauto = new JRadioButton("Auto");
+        Sauto.setSelected(true);
+        Sauto.setActionCommand("auto");
         JRadioButton Safficher = new JRadioButton("Affichée");
+        Safficher.setActionCommand("afficher");
         JRadioButton Scacher = new JRadioButton("Cachée");
+         Scacher.setActionCommand("cacher");
 
         JRadioButton Dauto = new JRadioButton("Auto");
+        Dauto.setSelected(true);
+        Dauto.setActionCommand("auto");
         JRadioButton Dafficher = new JRadioButton("Affichée");
+        Dafficher.setActionCommand("afficher");
         JRadioButton Dcacher = new JRadioButton("Cachée");
+        Dcacher.setActionCommand("cacher");
 
         JRadioButton Bauto = new JRadioButton("Auto");
+        Bauto.setActionCommand("auto");
+        Bauto.setSelected(true);
         JRadioButton Bafficher = new JRadioButton("Affichée");
+        Bafficher.setActionCommand("afficher");
         JRadioButton Bcacher = new JRadioButton("Cachée");
+        Bcacher.setActionCommand("cacher");
 
         grpS.add(Sauto);
         grpS.add(Safficher);
         grpS.add(Scacher);
 
-        grpD.add(Sauto);
-        grpD.add(Safficher);
-        grpD.add(Scacher);
+        grpD.add(Dauto);
+        grpD.add(Dafficher);
+        grpD.add(Dcacher);
 
-        grpB.add(Sauto);
-        grpB.add(Safficher);
-        grpB.add(Scacher);
+        grpB.add(Bauto);
+        grpB.add(Bafficher);
+        grpB.add(Bcacher);
 
         grp.add(auto);
         grp.add(afficher);
@@ -128,12 +156,59 @@ public class ConfigMenu extends JDialog {
     }
    private JPanel pnl_bas(){
         JPanel bas = new JPanel();
-        JButton valider = new JButton("Valider");
-        JButton annuler = new JButton("Annuler");
+        valider = new JButton("Valider");
+        valider.addActionListener(new ActionValider());
+        annuler = new JButton("Annuler");
         bas.add(valider);
         bas.add(annuler);
         return bas;
     }
+  public class ActionValider implements ActionListener {
 
+
+       @Override
+       public void actionPerformed(ActionEvent actionEvent) {
+            String actionConvertion = grp.getSelection().getActionCommand();
+            String actionSaisie = grpS.getSelection().getActionCommand();
+            String actionDiaporama = grpD.getSelection().getActionCommand();
+            String actionBouton = grpB.getSelection().getActionCommand();
+
+           if(actionConvertion=="cacher"){
+
+               application.masquerConversion(false);
+
+
+           }else if(actionConvertion == "afficher") {
+               application.enableConversion(true);
+
+           }
+
+           if(actionSaisie=="cacher"){
+
+               application.masquerSaisie(false);
+
+
+           }else if(actionSaisie == "afficher") {
+               application.masquerSaisie(true);
+
+           }
+
+           if(actionDiaporama=="cacher"){
+               application.masquerDiaporama(false);
+
+           }else if(actionDiaporama == "afficher") {
+                application.masquerDiaporama(true);
+           }
+
+           if(actionBouton=="cacher"){
+              application.masquerBoutons(false);
+
+           }else if(actionBouton == "afficher") {
+              application.masquerBoutons(true);
+
+           }
+           dispose();
+       }
+  }
 
 }
