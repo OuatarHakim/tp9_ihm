70dc3e99e7c9077bb8b90fe0a0e06e55b88cbb33
Hakim ouatar <hakimouatar6@gmail.com>
2021-03-27 08:35:55 +0100

    refactor

--- src/main/java/edu/mermet/tp8/Bdialogue/DialogCommentFaire.java
+++ src/main/java/edu/mermet/tp8/Bdialogue/DialogCommentFaire.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class DialogCommentFaire extends JDialog {
     HashMap<String, String> keyValue;
     JEditorPane text;
-    JList liste;
+    JList<String> liste;
 
     public DialogCommentFaire(JFrame parent) {
         super(parent, "Comment faire", true);
@@ -33,21 +33,22 @@ public class DialogCommentFaire extends JDialog {
          DefaultListModel<String> choix = new DefaultListModel<>();
          keyValue =new HashMap<>();
          read("HowTo/titres.properties");
-        Iterator iterator = read("HowTo/titres.properties").iterator();
+        Iterator<String> iterator = read("HowTo/titres.properties").iterator();
+
         while(iterator.hasNext()){
-            String key = (String) iterator.next();
+            String key = iterator.next();
             String value = readProp("HowTo/titres.properties",key);
             keyValue.put(value,key);
             choix.addElement(readProp("HowTo/titres.properties",key));
 
         }
 
-        liste = new JList(choix);
+        liste = new JList<>(choix);
         liste.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         liste.addListSelectionListener(listSelectionEvent -> {
 
 
-            String titreGuide = (String) liste.getSelectedValue();
+            String titreGuide =  liste.getSelectedValue();
             System.out.println(titreGuide);
             String key = keyValue.get(titreGuide);
             System.out.println(key);
